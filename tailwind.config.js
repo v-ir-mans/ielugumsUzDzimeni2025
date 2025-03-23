d25881b3961e19c147cecf7e48bdb230d60f3c35
Vagalēns <vagalehns@proton.me>
2025-03-23 14:26:51 +0200

    Almost done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx, astro}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [require("daisyui")],
+
+  }
+  
